01af6f5a8124c7778d68307c670dca4a202d04a3
Uzias Rivera <uziasrivera@gmail.com>
2019-10-16 10:10:01 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Web 23  is the bomb</h1>
       <h2>
         Deployed with{' '}
         <a
